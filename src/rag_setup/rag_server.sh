630644760f748da44fd38a3f9d66246ab55c8f0c
bachvudinh <bachvudinh02@gmail.com>
2025-06-05 08:01:07 +0000

    clean up the repo

--- src/rag_setup/rag_server.sh
+++ src/rag_setup/rag_server.sh
@@ -1,4 +1,4 @@
-corpus_file=/home/jovyan/visual-thinker-workspace/deep-research/data/processed/corpus.jsonl # jsonl
+corpus_file=index_musique_db/corpus.jsonl # jsonl
 save_dir=index_musique_db
 retriever_name=e5 # this is for indexing naming
 retriever_model=intfloat/e5-base-v2
@@ -12,6 +12,6 @@ python flashrag_server.py \
     --retriever_model $retriever_model \
     --reranking_topk 5 \
     --reranker_model "cross-encoder/ms-marco-MiniLM-L12-v2" \
-    --reranker_batch_size 32 \
+    --reranker_batch_size 64 \
     --host "0.0.0.0" \
     --port 2223
